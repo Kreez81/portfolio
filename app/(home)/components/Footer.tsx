1983828399c964b43ef9c1ee0bd63dca0e0c4243
Karan Pawar <110679627+Kreez81@users.noreply.github.com>
2024-04-04 12:15:22 +0530

    Complete

--- app/(home)/components/Footer.tsx
+++ app/(home)/components/Footer.tsx
@@ -4,6 +4,7 @@ import Navbar from './Navbar'
 export default function Footer() {
   return (
     <div className="border-t mt-20">
+        <div className="flex items-center justify-center text-center text-gray-400 mt-5">Living, learning, & leveling up one day at a time.</div>
         <Navbar className="flex-col gap-6"/>
     </div>
   )
